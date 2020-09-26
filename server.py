a222b5cadc6290698ac598dc59e59079d805e56f
martinsjohn <44913517+martinsjohn@users.noreply.github.com>
2020-09-26 12:53:11 -0400

    Server File for connection

---
+++ server.py
@@ -0,0 +1,52 @@
+import socket
+import threading
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#serv = socket.gethostbyname(socket.gethostname())
+serv = '0.0.0.0'
+port = 5959
+addr = (serv, 5959)
+s.bind(addr)
+
+
+
+def handle_client(conn, addr):
+    print(f"New Connection ----> {addr} connected")
+    
+    connected = True
+    while connected:
+        conn.send(bytes("YeeeHAw I'm The Blind Bandit and I'm gonna eat you","utf-8"))
+        msg = conn.recv(1024).decode("utf-8")
+        print(msg)
+        
+    
+    conn.close()
+    
+def start():
+    s.listen()
+    print(f"Listening on {serv}")
+    while True:
+        conn, addr = s.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"Active Connections ----> {threading.activeCount() - 4}")
+
+print ("Server Starting.......")
+start()
+    
+
+
+
+
+
+
+
+
+#2 players 
+s.listen(2) 
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} successful.")
+    clientsocket.send(bytes("James smells weird!", "utf-8"))
